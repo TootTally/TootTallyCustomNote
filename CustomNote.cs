910a834fe4087480b079d55eac733b25ea4cfc12
Electrostats <Super_justy@hotmail.com>
2023-11-20 07:49:04 -0500

    Custom Note Implemented

---
+++ CustomNote.cs
@@ -0,0 +1,187 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+namespace TootTallyCustomNote
+{
+    public static class CustomNote
+    {
+        private static Sprite _noteOriginalInTexture, _noteOriginalOutTexture;
+
+        private static Sprite _noteStartOutTexture, _noteEndOutTexture;
+        private static Sprite _noteStartInTexture, _noteEndInTexture;
+        private static string _lastNoteName;
+        private static readonly Random _rdm = new Random();
+
+        public static void LoadNoteTexture(GameController __instance, string NoteName)
+        {
+            //If textures are already set, skip
+            if (AreAllTexturesLoaded() && !ConfigNotesNameChanged()) return;
+
+            string folderPath = Path.Combine(Paths.BepInExRootPath, Plugin.NOTES_FOLDER_PATH, NoteName);
+
+            //Dont know which will request will finish first...
+            Plugin.Instance.StartCoroutine(LoadNoteTexture(folderPath + "/NoteStartOutline.png", texture =>
+            {
+                _noteStartOutTexture = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2f);
+                Plugin.LogInfo("NoteStartOutline Texture Loaded.");
+                if (AreAllTexturesLoaded() && __instance != null)
+                    OnAllTextureLoadedAfterConfigChange(__instance);
+            }));
+            Plugin.Instance.StartCoroutine(LoadNoteTexture(folderPath + "/NoteEndOutline.png", texture =>
+            {
+                _noteEndOutTexture = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2f);
+                Plugin.LogInfo("NoteEndOutline Texture Loaded.");
+                if (AreAllTexturesLoaded() && __instance != null)
+                    OnAllTextureLoadedAfterConfigChange(__instance);
+            }));
+            Plugin.Instance.StartCoroutine(LoadNoteTexture(folderPath + "/NoteStartInline.png", texture =>
+            {
+                _noteStartInTexture = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2f);
+                Plugin.LogInfo("NoteStartInline Texture Loaded.");
+                if (AreAllTexturesLoaded() && __instance != null)
+                    OnAllTextureLoadedAfterConfigChange(__instance);
+            }));
+            Plugin.Instance.StartCoroutine(LoadNoteTexture(folderPath + "/NoteEndInline.png", texture =>
+            {
+                _noteEndInTexture = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2f);
+                Plugin.LogInfo("NoteEndInline Texture Loaded.");
+                if (AreAllTexturesLoaded() && __instance != null)
+                    OnAllTextureLoadedAfterConfigChange(__instance);
+            }));
+        }
+
+        public static void UnloadTextures()
+        {
+            Texture2D.DestroyImmediate(_noteStartOutTexture);
+            Texture2D.DestroyImmediate(_noteEndOutTexture);
+            Texture2D.DestroyImmediate(_noteStartInTexture);
+            Texture2D.DestroyImmediate(_noteEndInTexture);
+            Plugin.LogInfo("Custom Notes Textures Destroyed.");
+        }
+
+        public static void OnAllTextureLoadedAfterConfigChange(GameController __instance)
+        {
+            ApplyCustomTextureToNotes(__instance);
+            _lastNoteName = Plugin.Instance.option.NoteName.Value;
+
+        }
+
+        public static bool AreAllTexturesLoaded() => _noteStartOutTexture != null && _noteEndOutTexture != null && _noteStartInTexture != null && _noteEndInTexture != null;
+
+        public static bool ConfigNotesNameChanged() => Plugin.Instance.option.NoteName.Value != _lastNoteName;
+
+        public static IEnumerator<UnityWebRequestAsyncOperation> LoadNoteTexture(string filePath, Action<Texture2D> callback)
+        {
+            UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(filePath);
+            yield return webRequest.SendWebRequest();
+
+            if (!webRequest.isNetworkError && !webRequest.isHttpError)
+                callback(DownloadHandlerTexture.GetContent(webRequest));
+            else
+                Plugin.LogInfo("Custom Note does not exist or have the wrong format.");
+        }
+
+        public static void ResolvePresets(GameController __instance)
+        {
+            if ((!AreAllTexturesLoaded() || __instance == null) && Plugin.Instance.option.NoteName.Value != Plugin.DEFAULT_NOTENAME)
+            {
+                Plugin.LogInfo($"[{Plugin.Instance.option.NoteName.Value}] preset loading...");
+                LoadNoteTexture(__instance, Plugin.Instance.option.NoteName.Value);
+            }
+            else if (Plugin.Instance.option.NoteName.Value != Plugin.DEFAULT_NOTENAME)
+                ApplyCustomTextureToNotes(__instance);
+            else if (__instance != null)
+            {
+                SetToOriginalTexture(__instance);
+                Plugin.LogInfo("[Default] preset selected. Not loading any Custom Notes.");
+            }
+        }
+
+        public static void ApplyCustomTextureToNotes(GameController __instance)
+        {
+            if (!AreAllTexturesLoaded()) return;
+            Plugin.LogInfo("Applying Custom Textures to notes.");
+
+            var design = __instance.singlenote.GetComponent<NoteDesigner>();
+            if (_noteOriginalInTexture == null || _noteOriginalOutTexture == null)
+            {
+                _noteOriginalInTexture = design.startdot.sprite;
+                _noteOriginalOutTexture = __instance.singlenote.transform.Find("StartPoint").GetComponent<Image>().sprite;
+            }
+
+            __instance.singlenote.transform.Find("StartPoint").GetComponent<Image>().sprite = _noteStartOutTexture;
+            __instance.singlenote.transform.Find("EndPoint").GetComponent<Image>().sprite = _noteEndOutTexture;
+            design.startdot.sprite = _noteStartInTexture;
+            design.enddot.sprite = _noteEndInTexture;
+
+        }
+
+        public static void SetToOriginalTexture(GameController __instance)
+        {
+            if (_noteOriginalInTexture == null || _noteOriginalOutTexture == null) return;
+
+            var design = __instance.singlenote.GetComponent<NoteDesigner>();
+            __instance.singlenote.transform.Find("StartPoint").GetComponent<Image>().sprite = _noteOriginalOutTexture;
+            __instance.singlenote.transform.Find("EndPoint").GetComponent<Image>().sprite = design.startdot.sprite = design.enddot.sprite = _noteOriginalInTexture;
+        }
+
+
+        public static void ApplyColor(GameController __instance)
+        {
+            Color c = Plugin.Instance.option.NoteColorStart.Value;
+            Color c2 = Plugin.Instance.option.NoteColorEnd.Value;
+            __instance.note_c_start = new float[] { c.r, c.g, c.b };
+            __instance.note_c_end = new float[] { c2.r, c2.g, c2.b };
+        }
+
+        public static void ApplyRandomColor(ref float col_r, ref float col_g, ref float col_b, ref float col_r2, ref float col_g2, ref float col_b2)
+        {
+            col_r = (float)_rdm.NextDouble();
+            col_g = (float)_rdm.NextDouble();
+            col_b = (float)_rdm.NextDouble();
+            col_r2 = (float)_rdm.NextDouble();
+            col_g2 = (float)_rdm.NextDouble();
+            col_b2 = (float)_rdm.NextDouble();
+        }
+
+        public static Color GetColorFromPosition(float pos)
+        {
+            var normalizedPos = 1f - ((pos + 180f) / 360f);
+            return new Color(1f - normalizedPos, normalizedPos, Math.Abs(2f * normalizedPos - 1f));
+        }
+
+        public static void ApplyNoteResize(GameController __instance)
+        {
+            var startRect = __instance.singlenote.transform.Find("StartPoint").GetComponent<RectTransform>();
+            var endRect = __instance.singlenote.transform.Find("EndPoint").GetComponent<RectTransform>();
+
+            startRect.sizeDelta = Plugin.Instance.option.NoteHeadSize.Value * Vector2.one * 40f;
+            endRect.sizeDelta = Plugin.Instance.option.NoteHeadSize.Value * Vector2.one * 17f;
+            startRect.pivot = endRect.pivot = Vector2.one / 2f;
+            startRect.anchoredPosition = Vector2.zero;
+
+            __instance.singlenote.transform.Find("StartPoint/StartPointColor").GetComponent<RectTransform>().sizeDelta = Plugin.Instance.option.NoteHeadSize.Value * Vector2.one * 16f;
+            __instance.singlenote.transform.Find("EndPoint/EndPointColor").GetComponent<RectTransform>().sizeDelta = Plugin.Instance.option.NoteHeadSize.Value * Vector2.one * 10f;
+
+            __instance.singlenote.transform.Find("Line").GetComponent<LineRenderer>().widthMultiplier = Plugin.Instance.option.NoteBodySize.Value * 7;
+            __instance.singlenote.transform.Find("OutlineLine").GetComponent<LineRenderer>().widthMultiplier = Plugin.Instance.option.NoteBodySize.Value * 12;
+        }
+
+        //The fact I have to do that is bullshit
+        public static void FixNoteEndPosition(GameController __instance)
+        {
+            foreach (GameObject note in __instance.allnotes)
+            {
+                var rect = note.transform.transform.Find("EndPoint").GetComponent<RectTransform>();
+                rect.anchorMin = rect.anchorMax = new Vector2(1, .5f);
+                rect.pivot = new Vector2(0.34f, 0.5f);
+            }
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -2,12 +2,14 @@
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using TootTallyCore.Utils.TootTallyModules;
 using TootTallySettings;
 using UnityEngine;
 
-namespace TootTally.ModuleTemplate
+namespace TootTallyCustomNote
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInDependency("TootTallyCore", BepInDependency.DependencyFlags.HardDependency)]
@@ -16,14 +18,17 @@ namespace TootTally.ModuleTemplate
     {
         public static Plugin Instance;
 
-        private const string CONFIG_NAME = "ModuleTemplate.cfg";
+        private const string CONFIG_NAME = "CustomNote.cfg";
+        private const string NOTE_CONFIG_FIELD = "CustomNote";
+        public static string NOTES_FOLDER_PATH = "CustomNotes";
+        public const string DEFAULT_NOTENAME = "Default";
         public Options option;
         private Harmony _harmony;
         public ConfigEntry<bool> ModuleConfigEnabled { get; set; }
         public bool IsConfigInitialized { get; set; }
 
         //Change this name to whatever you want
-        public string Name { get => PluginInfo.PLUGIN_NAME; set => Name = value; }
+        public string Name { get => "Custom Note"; set => Name = value; }
 
         public static TootTallySettingPage settingPage;
 
@@ -42,7 +47,7 @@ namespace TootTally.ModuleTemplate
         private void TryInitialize()
         {
             // Bind to the TTModules Config for TootTally
-            ModuleConfigEnabled = TootTallyCore.Plugin.Instance.Config.Bind("Modules", "<insert module name here>", true, "<insert module description here>");
+            ModuleConfigEnabled = TootTallyCore.Plugin.Instance.Config.Bind("Modules", "Custom Note", true, "Enable Custom Note Module");
             TootTallySettings.Plugin.Instance.AddModuleToSettingPage(this);
             TootTallyModuleManager.AddModule(this);
         }
@@ -53,19 +58,34 @@ namespace TootTally.ModuleTemplate
             ConfigFile config = new ConfigFile(configPath + CONFIG_NAME, true);
             option = new Options()
             {
-                // Set your config here by binding them to the related ConfigEntry in your Options class
-                // Example:
-                // Unlimited = config.Bind(CONFIG_FIELD, "Unlimited", DEFAULT_UNLISETTING)
+                NoteName = config.Bind(NOTE_CONFIG_FIELD, nameof(option.NoteName), DEFAULT_NOTENAME),
+                NoteHeadSize = config.Bind(NOTE_CONFIG_FIELD, nameof(option.NoteHeadSize), 1f, "Size of the start and end note circles"),
+                NoteBodySize = config.Bind(NOTE_CONFIG_FIELD, nameof(option.NoteBodySize), 1f, "Size of the note line"),
+                RandomNoteColor = config.Bind(NOTE_CONFIG_FIELD, nameof(option.RandomNoteColor), false, "Randomize all the colors of the notes"),
+                OverwriteNoteColor = config.Bind(NOTE_CONFIG_FIELD, nameof(option.OverwriteNoteColor), false, "Make the note color consistent"),
+                NoteColorStart = config.Bind(NOTE_CONFIG_FIELD, nameof(option.NoteColorStart), Color.white),
+                NoteColorEnd = config.Bind(NOTE_CONFIG_FIELD, nameof(option.NoteColorEnd), Color.black),
             };
 
-            settingPage = TootTallySettingsManager.AddNewPage("ModulePageName", "HeaderText", 40f, new Color(0,0,0,0));
-            if (settingPage != null) {
-                // Use TootTallySettingPage functions to add your objects to TootTallySetting
-                // Example:
-                // page.AddToggle(name, option.Unlimited);
-            }
+            TryMigrateFolder("CustomNotes");
 
-            _harmony.PatchAll(typeof(ModuleTemplatePatches));
+            settingPage = TootTallySettingsManager.AddNewPage("Custom Note", "Custom Note", 40f, new Color(0,0,0,0));
+            CreateDropdownFromFolder(NOTES_FOLDER_PATH, option.NoteName, DEFAULT_NOTENAME);
+
+            var headSlider = settingPage.AddSlider("NoteHeadSizeSlider", 0f, 5f, 250f, "Note Head Size", option.NoteHeadSize, false);
+            var bodySlider = settingPage.AddSlider("NoteBodySizeSlider", 0f, 5f, 250f, "Note Body Size", option.NoteBodySize, false);
+
+            settingPage.AddButton("ResetSliders", new Vector2(160, 80), "Reset", () =>
+            {
+                headSlider.slider.value = 1f;
+                bodySlider.slider.value = 1f;
+            });
+
+            settingPage.AddToggle("RandomNoteColor", option.RandomNoteColor);
+            settingPage.AddToggle("OverwriteNoteColor", option.OverwriteNoteColor, OnToggleValueChange);
+            if (option.OverwriteNoteColor.Value) OnToggleValueChange(true);
+
+            _harmony.PatchAll(typeof(CustomNotePatches));
             LogInfo($"Module loaded!");
         }
 
@@ -76,16 +96,111 @@ namespace TootTally.ModuleTemplate
             LogInfo($"Module unloaded!");
         }
 
-        public static class ModuleTemplatePatches
+        public void TryMigrateFolder(string folderName)
         {
-            // Apply your Trombone Champ patches here
+            string targetFolderPath = Path.Combine(Paths.BepInExRootPath, folderName);
+            if (!Directory.Exists(targetFolderPath))
+            {
+                string sourceFolderPath = Path.Combine(Path.GetDirectoryName(Plugin.Instance.Info.Location), folderName);
+                LogInfo($"{folderName} folder not found. Attempting to move folder from " + sourceFolderPath + " to " + targetFolderPath);
+                if (Directory.Exists(sourceFolderPath))
+                    Directory.Move(sourceFolderPath, targetFolderPath);
+                else
+                {
+                    LogError($"Source {folderName} Folder Not Found. Cannot Create {folderName} Folder. Download the module again to fix the issue.");
+                    return;
+                }
+            }
+        }
+
+        public void CreateDropdownFromFolder(string folderName, ConfigEntry<string> config, string defaultValue)
+        {
+            var folderNames = new List<string> { defaultValue };
+            var folderPath = Path.Combine(Paths.BepInExRootPath, folderName);
+            if (Directory.Exists(folderPath))
+            {
+                var directories = Directory.GetDirectories(folderPath).ToList();
+                directories.ForEach(d =>
+                {
+                    if (!d.Contains("TEMPALTE"))
+                        folderNames.Add(Path.GetFileNameWithoutExtension(d));
+                });
+            }
+            settingPage.AddLabel(folderName, folderName, 24, TMPro.FontStyles.Normal, TMPro.TextAlignmentOptions.BottomLeft);
+            settingPage.AddDropdown($"{folderName}Dropdown", config, folderNames.ToArray());
+        }
+
+        public void OnToggleValueChange(bool value)
+        {
+            if (value)
+            {
+                settingPage.AddLabel("Note Start Color");
+                settingPage.AddColorSliders("NoteStart", "Note Start Color", option.NoteColorStart);
+                settingPage.AddLabel("Note End Color");
+                settingPage.AddColorSliders("NoteEnd", "Note End Color", option.NoteColorEnd);
+            }
+            else
+            {
+                settingPage.RemoveSettingObjectFromList("Note Start Color");
+                settingPage.RemoveSettingObjectFromList("NoteStart");
+                settingPage.RemoveSettingObjectFromList("Note End Color");
+                settingPage.RemoveSettingObjectFromList("NoteEnd");
+            }
+
+        }
+
+        public static class CustomNotePatches
+        {
+            [HarmonyPatch(typeof(HomeController), nameof(HomeController.tryToSaveSettings))]
+            [HarmonyPostfix]
+            public static void OnSettingsChange()
+            {
+               CustomNote.ResolvePresets(null);
+            }
+
+            [HarmonyPatch(typeof(HomeController), nameof(HomeController.Start))]
+            [HarmonyPostfix]
+            public static void OnHomeStartLoadTexture()
+            {
+                CustomNote.ResolvePresets(null);
+            }
+
+            [HarmonyPatch(typeof(GameController), nameof(GameController.buildNotes))]
+            [HarmonyPrefix]
+            public static void PatchCustorTexture(GameController __instance)
+            {
+                CustomNote.ResolvePresets(__instance);
+                CustomNote.ApplyNoteResize(__instance);
+
+                if (Instance.option.OverwriteNoteColor.Value)
+                    CustomNote.ApplyColor(__instance);
+            }
+
+            [HarmonyPatch(typeof(NoteDesigner), nameof(NoteDesigner.setColorScheme))]
+            [HarmonyPrefix]
+            public static void PatchCursorColorRandom(ref float col_r, ref float col_g, ref float col_b, ref float col_r2, ref float col_g2, ref float col_b2)
+            {
+                if (Instance.option.RandomNoteColor.Value)
+                    CustomNote.ApplyRandomColor(ref col_r, ref col_g, ref col_b, ref col_r2, ref col_g2, ref col_b2);
+            }
+
+            [HarmonyPatch(typeof(GameController), nameof(GameController.buildNotes))]
+            [HarmonyPostfix]
+            public static void FixEndNotePosition(GameController __instance)
+            {
+                CustomNote.FixNoteEndPosition(__instance);
+            }
         }
 
         public class Options
         {
-            // Fill this class up with ConfigEntry objects that define your configs
-            // Example:
-            // public ConfigEntry<bool> Unlimited { get; set; }
+            public ConfigEntry<string> NoteName { get; set; }
+            public ConfigEntry<float> NoteHeadSize { get; set; }
+            public ConfigEntry<float> NoteBodySize { get; set; }
+            public ConfigEntry<bool> RandomNoteColor { get; set; }
+            public ConfigEntry<bool> OverwriteNoteColor { get; set; }
+            public ConfigEntry<Color> NoteColorStart { get; set; }
+            public ConfigEntry<Color> NoteColorEnd { get; set; }
         }
     }
 }
